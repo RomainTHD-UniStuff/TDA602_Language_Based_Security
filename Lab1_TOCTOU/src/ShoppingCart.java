8d77f0eaf44ccbd663ec8569b8017a0ede6f9b4f
RomainTHD <romain-thd+github@protonmail.com>
2022-03-28 15:07:40 +0200

    feat: First exploit

--- Lab1_TOCTOU/src/ShoppingCart.java
+++ Lab1_TOCTOU/src/ShoppingCart.java
@@ -5,26 +5,41 @@ import backend.Wallet;
 import java.util.Scanner;
 
 public class ShoppingCart {
-    private static void print(Wallet wallet, Pocket pocket) throws Exception {
+    private static Scanner scanner;
+
+    private static void print(Wallet wallet, Pocket pocket) {
+        System.out.println("\n");
         System.out.println("Your current balance is: " + wallet.getBalance() + " credits.");
         System.out.println(Store.asString());
         System.out.println("Your current pocket is:\n" + pocket.getPocket());
     }
 
-    private static String scan(Scanner scanner) throws Exception {
-        System.out.print("What do you want to buy? (type quit to stop) ");
+    private static String scan() {
+        System.out.print("What do you want to buy? (empty line to stop) > ");
         return scanner.nextLine();
     }
 
-    public static void main(String[] args) throws Exception {
+    private static boolean shouldQuit(String msg) {
+        return msg.equals("quit")
+               || msg.equals("exit")
+               || msg.equals("q")
+               || msg.equals("");
+    }
+
+    private static void delay() {
+        System.out.println("(waiting for input to resume...)");
+        scanner.nextLine();
+    }
+
+    public static void main(String[] args) {
         Wallet wallet = new Wallet();
         Pocket pocket = new Pocket();
-        Scanner scanner = new Scanner(System.in);
+        scanner = new Scanner(System.in);
 
         print(wallet, pocket);
-        String product = scan(scanner);
+        String product;
 
-        while (!product.equals("quit")) {
+        while (!shouldQuit(product = scan().trim().toLowerCase())) {
             /* TODO:
                - check if the amount of credits is enough, if not stop the execution.
                - otherwise, withdraw the price of the product from the wallet.
@@ -32,9 +47,32 @@ public class ShoppingCart {
                - print the new balance.
             */
 
+            Integer price = Store.getProductPrice(product);
+
+            if (price == null) {
+                System.out.println("Product not found.");
+                continue;
+            }
+
+            int balance = wallet.getBalance();
+
+            delay();
+            /*
+            Step 1: start buying a candy, credits = 30000
+            Step 2: buy the car, credits = 0
+            Step 3: resume buying the candy, credits = 29999
+            */
+
+            if (price > balance) {
+                System.out.println("You don't have enough credits to buy this product.");
+                continue;
+            } else {
+                wallet.setBalance(balance - price);
+                pocket.addProduct(product);
+            }
+
             // Just to print everything again...
             print(wallet, pocket);
-            product = scan(scanner);
         }
     }
 }
